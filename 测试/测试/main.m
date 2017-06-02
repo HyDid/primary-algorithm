2dfb7ea3b56e62145871d1ad47ad61265bc7f540
LiHongYu <2443077810@qq.com>
2017-06-02 15:10:12 +0800

    快排

--- 测试/测试/main.m
+++ 测试/测试/main.m
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 
-
+//打印数组
 void pout(int arr[] ,int a){
     for (int i = 0; i<a; i++) {
         printf("%i",arr[i]);
@@ -17,13 +17,14 @@ void pout(int arr[] ,int a){
 }
 
 
-
+/*
+ 稳定排序：插入，冒泡，选择。
+ 平均时间复杂度：O(n^2)
+*/
 
 //插入排序
 void insertSort(int arr[],int a ){
-    
-     //取出值的前位比取出值大便向后移一位知道循环结束，此时j数值为取出值应在位前一位。
-
+     //取出值的前位比取出值大便向后移一位直到循环结束，此时j数值为取出值应在位前一位。
     for (int i = 1;i<a ;i++) {
         
         int v = arr[i];//取出值
@@ -35,19 +36,34 @@ void insertSort(int arr[],int a ){
         }
         
         arr[j+1] = v;
-        
     }
-    
+
     pout(arr,a);
-    
 }
 
-//冒泡排序
+//希尔排序
+void hill(int arr[] ,int a){
+    int d;
+    
+    for ( d =a/2; d>=1; d= d/2) {
+        for (int i = d+1; i<=a ; i++) {
+            int k = arr[i];
+            int j;
+            for (j = i-d; j>0&&k<arr[j]; j=j-d) {
+                arr[j+d] = arr[j];
+            }
+            arr[j+d] = k;
+            
+            
+        }
+    }
+}
 
+
+//冒泡排序
 void bubble(int arr[] , int a ){
     
     for (int i = 1; i<a; i++) {
-        
         //j>=i+1是因为只进行未排序部分的比较
         for (int j = a - 1; j>=i; j--) {
             
@@ -56,22 +72,14 @@ void bubble(int arr[] , int a ){
                 arr[j] = arr[j-1];
                 arr[j-1] = k;
             }
-            
         }
-        
-        
     }
-    
-    
     pout(arr, a);
 }
 
 
 //选择排序
-
 void selection(int arr[],int a){
-    
-    
     
     for (int i = 0; i<a-1; i++) {
         
@@ -82,15 +90,13 @@ void selection(int arr[],int a){
             if (arr[j]<arr[min]) {
                 min = j;
             }
-            
         }
         
         int t = arr[i];
         arr[i] = arr[min];
         arr[min] = t;
     }
-    
-    
+
     pout(arr, a);
 }
 
@@ -123,17 +129,13 @@ void merge(int A[],int left ,int mid ,int right){
 
 void mersort(int A[],int left ,int right){
 
-    if(left+1 < right){
-    int mid = (left+right)/2;
-    
-    mersort(A, left, mid);
-    mersort(A, mid, right);
-    merge(A, left, mid, right);
-    
-    
+    if (left+1 < right) {
+        
+        int mid = (left+right)/2;
+        mersort(A, left, mid);
+        mersort(A, mid, right);
+        merge(A, left, mid, right);
     }
-    
-    
 }
 
 
@@ -141,7 +143,6 @@ void mersort(int A[],int left ,int right){
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
         
         int arr[]={9,5,3,2,4,4,6,9,2};
         int a = sizeof(arr)/sizeof(arr[0]);//数组占内存总空间除以单个元素占内存空间大小，即等于元素个数
